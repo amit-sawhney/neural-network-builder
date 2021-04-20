997034a34b28a0c99da76605933947ed4b7a6258
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-20 14:39:45 -0500

    Add sigmoid testing

--- tests/core/test_model.cc
+++ tests/core/test_model.cc
@@ -1,12 +1,1 @@
 #include <catch2/catch.hpp>
-
-TEST_CASE("Sanity Test") {
-  
-  SECTION("Sanity Section 1") {
-    REQUIRE(2 < 1);
-  }
-  
-  SECTION("Sanity Section 2") {
-    REQUIRE(2 > 1);
-  }
-}
--- tests/utils/test_model_math.cc
+++ tests/utils/test_model_math.cc
@@ -1,10 +1,37 @@
 #include <catch2/catch.hpp>
 
+#include "utils/model_math.h"
+
+using neural_network::ModelMath;
+
+const float kCalculationTolerance = 0.1f;
+
 TEST_CASE("Sigmoid Calculations") {
 
-  SECTION("Negative value") {}
+  SECTION("Negative value") {
+    float input = -1;
 
-  SECTION("Zero input") {}
+    float sigmoid = ModelMath::CalculateSigmoid(input);
+    float expected_value = 0.2689f;
 
-  SECTION("Positive value") {}
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+
+  SECTION("Zero input") {
+    float input = 0;
+
+    float sigmoid = ModelMath::CalculateSigmoid(input);
+    float expected_value = 0.5f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+
+  SECTION("Positive value") {
+    float input = 1;
+
+    float sigmoid = ModelMath::CalculateSigmoid(input);
+    float expected_value = 0.73106f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
 }
