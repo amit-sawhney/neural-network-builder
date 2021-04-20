54ca6e093d380a63b0246ab4ba965f1240c7f25e
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-20 15:34:56 -0500

    Add derivative sigmoid testing

--- tests/utils/test_model_math.cc
+++ tests/utils/test_model_math.cc
@@ -4,11 +4,11 @@
 
 using neural_network::ModelMath;
 
-const float kCalculationTolerance = 0.1f;
+const float kCalculationTolerance = 0.01f;
 
 TEST_CASE("Sigmoid Calculations") {
 
-  SECTION("Negative value") {
+  SECTION("Negative input") {
     float input = -1;
 
     float sigmoid = ModelMath::CalculateSigmoid(input);
@@ -26,11 +26,51 @@ TEST_CASE("Sigmoid Calculations") {
     REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
   }
 
-  SECTION("Positive value") {
+  SECTION("Positive input") {
     float input = 1;
 
     float sigmoid = ModelMath::CalculateSigmoid(input);
     float expected_value = 0.73106f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+}
+
+TEST_CASE("Sigmoid Derivative Calculations") {
+
+  SECTION("Negative input") {
+    float input = -1;
+
+    float sigmoid = ModelMath::CalculateSigmoidDerivative(input);
+    float expected_value = 0.1967f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+
+  SECTION("Zero input") {
+    float input = 0;
+
+    float sigmoid = ModelMath::CalculateSigmoidDerivative(input);
+    float expected_value = 0.25f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+
+  SECTION("Positive input") {
+    float input = 1;
+
+    float sigmoid = ModelMath::CalculateSigmoidDerivative(input);
+    float expected_value = 0.196f;
+
+    REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
+  }
+
+  SECTION("Sigmoid input") {
+    float input = ModelMath::CalculateSigmoid(1);
+    bool isSigmoid = true;
+
+    float sigmoid = ModelMath::CalculateSigmoidDerivative(input, isSigmoid);
+    float expected_value = 0.1967f;
 
     REQUIRE(sigmoid == Approx(expected_value).epsilon(kCalculationTolerance));
   }
