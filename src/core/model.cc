9f98fbd76eda69b4a4493d98205b942cfb19eddc
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-20 20:54:18 -0500

    Add TODO statement for future weeks

--- src/core/model.cc
+++ src/core/model.cc
@@ -21,10 +21,17 @@ Model::Model(const std::vector<size_t> &neuron_layers, float learning_rate) {
 
 std::ostream &operator<<(std::ostream &output, const Model &model) {
 
+  // TODO: Implement operator for week 3
+
   return output;
 }
 
-std::istream &operator>>(std::istream &input, Model &model) { return input; }
+std::istream &operator>>(std::istream &input, Model &model) {
+
+  // TODO: Implement operator for week 3
+
+  return input;
+}
 
 void Model::Clear() {
   num_neurons_ = 0;
