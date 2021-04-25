b98ca4108e546758bf797836bbd118bd6f4c951c
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-24 20:29:19 -0500

    Initialize cinder loading

--- include/visualizer/neural_network_builder.h
+++ include/visualizer/neural_network_builder.h
@@ -1,3 +1,22 @@
 #pragma once
 
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
 
+namespace neural_network {
+
+namespace visualizer {
+
+class NeuralNetworkBuilderApp : public ci::app::App {
+public:
+  NeuralNetworkBuilderApp();
+
+  void draw() override;
+
+  const float kWindowSize = 1075.0f;
+
+private:
+};
+} // namespace visualizer
+} // namespace neural_network
--- src/visualizer/neural_network_builder.cc
+++ src/visualizer/neural_network_builder.cc
@@ -1,5 +1,14 @@
-//
-// Created by asawh on 4/16/2021.
-//
-
 #include "visualizer/neural_network_builder.h"
+
+namespace neural_network {
+namespace visualizer {
+
+NeuralNetworkBuilderApp::NeuralNetworkBuilderApp() = default;
+
+void NeuralNetworkBuilderApp::draw() {}
+
+
+
+} // namespace visualizer
+
+} // namespace neural_network
